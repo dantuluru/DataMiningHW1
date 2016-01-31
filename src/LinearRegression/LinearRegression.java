52db18d9eab6f63c36d7c904b15cbeda87173df5
dantuluru <mounika@ccs.neu.edu>
2016-01-31 16:46:51 -0500

    Formula from slide implemented
    its getting into infinite loop

--- src/LinearRegression/LinearRegression.java
+++ src/LinearRegression/LinearRegression.java
@@ -61,103 +61,127 @@ public class LinearRegression {
      */
     private static Matrix getBeta(Matrix trainX, Matrix trainY) {
     	/****************Please Fill Missing Lines Here*****************/
+    	//get batch gradient descent beta
+    	int nCols = trainX.getColumnDimension();
+    	double eta = 0.005;
+    	Matrix oldBeta = new Matrix(1, nCols);
+    		for(int c=0; c<nCols; c++){
+    			oldBeta.set(0, c, 1);
+    		}
+    	Matrix newBeta = new Matrix(1,nCols);
+    	int n = 0;
+//    	while(n<1){
+         while(!compareBeta(oldBeta,newBeta)){
+    		oldBeta = newBeta;
+    		Matrix calMat = gradientBeta(trainX,trainY, oldBeta);
+    		newBeta = oldBeta.minus(calMat.timesEquals(eta));
+    		n++;
+    		System.out.println(n);
+    		if (n == 5) {
+    			break;
+    		}
+    	}
+    	//return newBeta;
+    	//Matrix xT = modifiedX(trainX).transpose();
+    	//Matrix result = xT.times(modifiedX(trainX)).inverse().times(xT.times(trainY));
+    	
+    	return newBeta.transpose(); 	
+    }
+    
+    private static boolean compareBeta(Matrix oldBeta, Matrix newBeta){
+    	oldBeta.print(0, 2);
+    	newBeta.print(0, 2);
+    	boolean test = true;
+    	int nCols = oldBeta.getColumnDimension();
+    	for(int c=0; c<nCols; c++){
+    		double oldB =oldBeta.get(0, c);
+    		double newB = newBeta.get(0, c);
+    		if( Math.abs(oldB - newB) < 0.001 ) { // abs(old - new) < eplsilon)Math.abs(d - 1.0) < 0.001);
+    			test = test & true;
+    		}
+    		else {
+    			test = test & false;
+    		}
+    	}
+    	System.out.println(test);
+    	return test;
+    }
+    
+    private static Matrix gradientBeta(Matrix trainX, Matrix trainY, Matrix beta){
     	int nRows = trainX.getRowDimension();
     	int nCols = trainX.getColumnDimension();
     	
-    	Matrix theta = new Matrix(nCols, 1);
-    	for (int r=0; r<nCols; r++) {
-        	theta.set(r, 0, 1);
-        }
-    	// Get Initial MSE
-    	Matrix predictedY = trainX.times(theta);
-    	double MSE = mean_square_error(predictedY,trainY);
-
-    	return gradient_descent(trainX, trainY, theta, 0.01, 100);
-    	//Matrix xT = modifiedX(trainX).transpose();
-    	//return xT.times(modifiedX(trainX)).inverse().times(xT.times(trainY)); 	
-    }
-    
-    private static double mean_square_error(Matrix predictedY, Matrix actualY){
-    	
-    	return MSE;
-    }
-    
-    private static Matrix gradient_descent(Matrix x, Matrix y, Matrix theta, double alpha, int num_iters){
-    	int m = y.getRowDimension();
-    	Matrix xTrans = x.transpose();
-    	
-    	for (int i=0; i<num_iters; i++) {
-    		Matrix hypothesis = x.times(theta);
-    		Matrix loss = hypothesis.minus(y);
-    		// formula for cost = (loss^2)/(2*m)
-    		Matrix cost = squareMat(loss,m);
-   		    Matrix gradient = calculateMat(xTrans, loss, m);
-   		    Matrix temp = matSub(theta, alpha);
-   		    theta = theta.minus(gradient.times(alpha));
-   		    
-   		    // Check for convergence
-   		    // Get new MSE, using trainX, theta
+    	Matrix xi = new Matrix(1, nCols);
+    	Matrix bi = new Matrix(1, nCols);
+    	Matrix sumXi = new Matrix(1, nCols);
+    	double yi = 0;
+    	for(int c=0; c<nCols; c++){
+			sumXi.set(0, c, 0);
+		}
+    	for (int r=0;r<nRows;r++){
+    		for(int c=0;c<nCols;c++){
+    			xi.set(0, c, trainX.get(r, c));  			
+    			bi.set(0, c, beta.get(0, c));
+    		}
+    		yi = trainY.get(r, 0);
+    		Matrix calMat = bi.times(xi.transpose());
+    		double calVal = calMat.get(0, 0);
+    		calVal -= yi;
+    		sumXi.plusEquals(xi.times(2 * calVal));
     	}
-        return theta;
-    }
+    	return sumXi;
+    } 
+    
+//    
+//    private static double mean_square_error(Matrix predictedY, Matrix actualY){
+//    	double MSE;
+//    	Matrix subMat = actualY.minus(predictedY);
+//    	for(r)
+//    	return MSE;
+//    }
+////    
+//    private static Matrix gradient_descent(Matrix x, Matrix y, Matrix theta, double alpha, int num_iters){
+//    	int m = y.getRowDimension();
+//    	Matrix xTrans = x.transpose();
+//    	
+//    	for (int i=0; i<num_iters; i++) {
+//    		Matrix hypothesis = x.times(theta);
+//    		Matrix loss = hypothesis.minus(y);
+//    		// formula for cost = (loss^2)/(2*m)
+//    		Matrix cost = squareMat(loss,m);
+//   		    Matrix gradient = calculateMat(xTrans, loss, m);
+//   		    Matrix temp = matSub(theta, alpha);
+//   		    theta = theta.minus(gradient.times(alpha));
+//   		    
+//   		    // Check for convergence
+//   		    // Get new MSE, using trainX, theta
+//    	}
+//        return theta;
+//    }
     //to subtract a scalar from matrix
-    private static Matrix matSub(Matrix A, double alpha){
-    	int nRows = A.getRowDimension();
-    	int nCols = A.getColumnDimension();
-    	for (int r=0; r<nRows; r++){
-    		for(int c=0; c<nCols; c++){
-    			A.set(r, c, A.get(r,c)-alpha);
-    		}
-    	}
-    	return A;
-    }
+//    private static Matrix matSub(Matrix A, double alpha){
+//    	int nRows = A.getRowDimension();
+//    	int nCols = A.getColumnDimension();
+//    	for (int r=0; r<nRows; r++){
+//    		for(int c=0; c<nCols; c++){
+//    			A.set(r, c, A.get(r,c)-alpha);
+//    		}
+//    	}
+//    	return A;
+//    }
     
-    private static Matrix squareMat(Matrix A, int m){
-    	int nRows = A.getRowDimension();
-    	int nCols = A.getColumnDimension();
-    	for (int r=0; r<nRows; r++){
-    		for(int c=0; c<nCols; c++){
-    			double calcValue = (A.get(r,c) * A.get(r,c))/(2 * m);
-    			A.set(r,c,calcValue);
-    		}
-    	}
-    return A;
-    }
-    
-    // to multiply two matrices and divide the result by a scalar
-    private static Matrix calculateMat(Matrix A,Matrix B, int m){
-    	Matrix multValue = A.times(B);
-    	int nRows = multValue.getRowDimension();
-    	int nCols = multValue.getColumnDimension();
-    	for (int r=0; r<nRows; r++){
-    		for(int c=0; c<nCols; c++){
-    			//double calVal = Math.round(multValue.get(r,c)/m * 10000.0)/10000.0;
-    			multValue.set(r,c,multValue.get(r,c)/m);
-    		}
-    	}
-    	return multValue;
-    }
-        // Performs gradient descent to learn theta
-        // by taking num_items gradient steps with learning
-        // rate alpha
-        
-//        int m = y.getRowDimension();
-//        Matrix J_history = new Matrix(num_iters, 1);
-//
-//        for (int i=0; i<num_iters; i++) {
-//
-//            Matrix predictions = X.times(theta);
-//
-//            Matrix errors_x1 = (predictions.minus(y)).times(X);
-//            Matrix errors_x2 = (predictions.minus(y)).times(X);
-//
-//            theta[0][0] = theta[0][0] - alpha * (1.0 / m) * errors_x1.sum();
-//            theta[1][0] = theta[1][0] - alpha * (1.0 / m) * errors_x2.sum();
-//
-//            J_history[i, 0] = compute_cost(X, y, theta)
-//        }
+//    private static Matrix squareMat(Matrix A, int m){
+//    	int nRows = A.getRowDimension();
+//    	int nCols = A.getColumnDimension();
+//    	for (int r=0; r<nRows; r++){
+//    		for(int c=0; c<nCols; c++){
+//    			double calcValue = (A.get(r,c) * A.get(r,c))/(2 * m);
+//    			A.set(r,c,calcValue);
+//    		}
+//    	}
+//    return A;
+//    }
 
- //   return theta, J_history
 
     /**
      * @params: predicted Y matrix
